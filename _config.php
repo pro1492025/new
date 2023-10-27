1c2eb4e5fc62a918a41019b8b6719decf1690321
pro1492025 <148998193+pro1492025@users.noreply.github.com>
2023-10-27 15:35:45 +0530

    Add files via upload

---
+++ _config.php
@@ -0,0 +1,31 @@
+<?php 
+$conn = mysqli_connect("localhost", 'root' , '', "anikatsu") or die("Connection fail");
+
+
+$websiteTitle = "AniKatsu"; // Website Name
+$websiteUrl = "//{$_SERVER['SERVER_NAME']}";  // Website URL
+$websiteLogo = $websiteUrl . "/files/images/logo.png?v=2"; // Logo
+$contactEmail = "xyz@gmail.com"; // Contact Email
+
+$version = "0.4";
+
+//Donate 
+$donate = "#";
+
+// Socials 
+$telegram = "https://t.me/#"; // telegram
+$discord = "https://discord.com/#"; // Discord
+$redit = "#"; // Reddit
+$twitter = "#"; // Twitter
+ 
+
+
+$disqus = "https://indianime.disqus.com"; // Disqus
+
+
+// API URL
+$api = "http://anikatsu.shashanktiwar11.repl.co"; // https://github.com/shashankktiwariii/anikatsu-api
+
+
+$banner = $websiteUrl . "/files/images/banner.png";  //Banner
+?>
